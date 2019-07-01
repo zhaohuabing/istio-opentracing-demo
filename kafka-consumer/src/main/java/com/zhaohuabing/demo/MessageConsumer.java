4c9fc9527c2c5df465f8472fe4e9c4c413ce3aee
Huabing Zhao <zhaohuabing@gmail.com>
2019-07-01 18:41:02 +0800

    call rest API when receiving kafka message
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>

--- kafka-consumer/src/main/java/com/zhaohuabing/demo/MessageConsumer.java
+++ kafka-consumer/src/main/java/com/zhaohuabing/demo/MessageConsumer.java
@@ -1,18 +1,30 @@
 package com.zhaohuabing.demo;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import io.opentracing.SpanContext;
+import io.opentracing.Tracer;
+import io.opentracing.contrib.kafka.TracingKafkaUtils;
 
 @Component
 public class MessageConsumer {
 
+    @Autowired
+    private Tracer tracer;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @KafkaListener(topics = "eshop-topic")
-    public void receive(String payload) {
-        try {
-            Thread.sleep((long) (Math.random() * 100));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void receiveMessage(ConsumerRecord<String, String> record, Acknowledgment acks) {
+        SpanContext spanContext = TracingKafkaUtils.extractSpanContext(record.headers(), tracer);
+        restTemplate.getForEntity("http://notification:8080/sendEmail", String.class);
         System.out.println("*** receive kafka message ***");
+        acks.acknowledge();
     }
 }
---
+++ rest-service/src/main/java/com/zhaohuabing/demo/service/NotificationServiceController.java
@@ -0,0 +1,24 @@
+package com.zhaohuabing.demo.service;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Huabing Zhao
+ */
+@RestController
+public class NotificationServiceController {
+
+    @RequestMapping(value = "/sendEmail")
+    public String payment(@RequestHeader HttpHeaders headers) throws InterruptedException {
+        try {
+            Thread.sleep((long) (Math.random() * 100));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Email sent out!");
+        return "Email sent out!";
+    }
+}
